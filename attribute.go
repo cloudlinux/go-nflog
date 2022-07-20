947d3d407e78e1aa39f72d4b0182885f7d0f035a
Nurzhan Bazhirov <nurzhanbazhirov@gmail.com>
2022-07-20 16:49:21 +0600

    use values instead of pointers in Attribute
    Signed-off-by: Nurzhan Bazhirov <nurzhanbazhirov@gmail.com>

--- attribute.go
+++ attribute.go
@@ -20,14 +20,11 @@ func extractAttribute(a *Attribute, logger *log.Logger, data []byte) error {
 	for ad.Next() {
 		switch ad.Type() {
 		case nfUlaAttrPacketHdr:
-			hwProtocol := binary.BigEndian.Uint16(ad.Bytes()[:2])
-			a.HwProtocol = &hwProtocol
-			hook := uint8(ad.Bytes()[3])
-			a.Hook = &hook
+			a.HwProtocol = binary.BigEndian.Uint16(ad.Bytes()[:2])
+			a.Hook = uint8(ad.Bytes()[3])
 		case nfUlaAttrMark:
 			ad.ByteOrder = binary.BigEndian
-			mark := ad.Uint32()
-			a.Mark = &mark
+			a.Mark = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrTimestamp:
 			var sec, usec int64
@@ -39,80 +36,61 @@ func extractAttribute(a *Attribute, logger *log.Logger, data []byte) error {
 			if err := binary.Read(r, binary.BigEndian, &usec); err != nil {
 				return err
 			}
-			timestamp := time.Unix(sec, usec*1000)
-			a.Timestamp = &timestamp
+			a.Timestamp = time.Unix(sec, usec*1000)
 		case nfUlaAttrIfindexIndev:
 			ad.ByteOrder = binary.BigEndian
-			inDev := ad.Uint32()
-			a.InDev = &inDev
+			a.InDev = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexOutdev:
 			ad.ByteOrder = binary.BigEndian
-			outDev := ad.Uint32()
-			a.OutDev = &outDev
+			a.OutDev = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexPhysIndev:
 			ad.ByteOrder = binary.BigEndian
-			physInDev := ad.Uint32()
-			a.PhysInDev = &physInDev
+			a.PhysInDev = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrIfindexPhysOutdev:
 			ad.ByteOrder = binary.BigEndian
-			physOutDev := ad.Uint32()
-			a.PhysOutDev = &physOutDev
+			a.PhysOutDev = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwaddr:
 			hwAddrLen := binary.BigEndian.Uint16(ad.Bytes()[:2])
-			hwAddr := (ad.Bytes())[4 : 4+hwAddrLen]
-			a.HwAddr = &hwAddr
+			a.HwAddr = (ad.Bytes())[4 : 4+hwAddrLen]
 		case nfUlaAttrPayload:
-			payload := ad.Bytes()
-			a.Payload = &payload
+			a.Payload = ad.Bytes()
 		case nfUlaAttrPrefix:
-			prefix := ad.String()
-			a.Prefix = &prefix
+			a.Prefix = ad.String()
 		case nfUlaAttrUID:
 			ad.ByteOrder = binary.BigEndian
-			uid := ad.Uint32()
-			a.UID = &uid
+			a.UID = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrSeq:
 			ad.ByteOrder = binary.BigEndian
-			seq := ad.Uint32()
-			a.Seq = &seq
+			a.Seq = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrSeqGlobal:
 			ad.ByteOrder = binary.BigEndian
-			seqGlobal := ad.Uint32()
-			a.SeqGlobal = &seqGlobal
+			a.SeqGlobal = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrGID:
 			ad.ByteOrder = binary.BigEndian
-			gid := ad.Uint32()
-			a.GID = &gid
+			a.GID = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwType:
 			ad.ByteOrder = binary.BigEndian
-			hwType := ad.Uint16()
-			a.HwType = &hwType
+			a.HwType = ad.Uint16()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrHwHeader:
-			hwHeader := ad.Bytes()
-			a.HwHeader = &hwHeader
+			a.HwHeader = ad.Bytes()
 		case nfUlaAttrHwLen:
 			ad.ByteOrder = binary.BigEndian
-			hwLen := ad.Uint16()
-			a.HwLen = &hwLen
+			a.HwLen = ad.Uint16()
 			ad.ByteOrder = nativeEndian
 		case nfUlaAttrCt:
-			ad.ByteOrder = binary.BigEndian
-			ct := ad.Bytes()
-			a.Ct = &ct
-			ad.ByteOrder = nativeEndian
+			a.Ct = ad.Bytes()
 		case nfUlaAttrCtInfo:
 			ad.ByteOrder = binary.BigEndian
-			ctInfo := ad.Uint32()
-			a.CtInfo = &ctInfo
+			a.CtInfo = ad.Uint32()
 			ad.ByteOrder = nativeEndian
 		default:
 			logger.Printf("Unknown attribute: %d %v\n", ad.Type(), ad.Bytes())
--- types.go
+++ types.go
@@ -88,26 +88,26 @@ const (
 // the elements inside Attribute are pointers to these values
 // or nil, if not present.
 type Attribute struct {
-	Hook       *uint8
-	Mark       *uint32
-	Timestamp  *time.Time
-	InDev      *uint32
-	PhysInDev  *uint32
-	OutDev     *uint32
-	PhysOutDev *uint32
-	Payload    *[]byte
-	Prefix     *string
-	UID        *uint32
-	Seq        *uint32
-	SeqGlobal  *uint32
-	GID        *uint32
-	HwType     *uint16
-	HwAddr     *[]byte
-	HwHeader   *[]byte
-	HwLen      *uint16
-	HwProtocol *uint16
-	CtInfo     *uint32
-	Ct         *[]byte
+	Hook       uint8
+	Mark       uint32
+	Timestamp  time.Time
+	InDev      uint32
+	PhysInDev  uint32
+	OutDev     uint32
+	PhysOutDev uint32
+	Payload    []byte
+	Prefix     string
+	UID        uint32
+	Seq        uint32
+	SeqGlobal  uint32
+	GID        uint32
+	HwType     uint16
+	HwAddr     []byte
+	HwHeader   []byte
+	HwLen      uint16
+	HwProtocol uint16
+	CtInfo     uint32
+	Ct         []byte
 }
 
 // Config contains options for a Conn.
